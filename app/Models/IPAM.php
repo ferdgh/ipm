8537c4ecc0ee5719157587d05c01203d72656c13
ferdinand borromeo <ferdinand.borromeo@gmail.com>
2023-07-18 02:58:42 +0800

    add IP logger on every user login

---
+++ app/Models/IPAM.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Auth;
+use DB;
+use Session;
+
+class IPAM
+{
+
+    public static function log_ip()
+    {
+        //get current IP Address
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+
+        //check
+        $ip = DB::table('ip_addresses')
+             ->select('id')
+             ->where('ip_address',$ip_address)
+             ->where('user_id',Auth::user()->id)
+             ->first();
+        if(isset($ip->id))
+        {
+            $ip_id = $ip->id;
+        }else{
+            //save new IP
+            $ip_id = DB::table('ip_addresses')
+                     ->insertGetId([
+                        'ip_address'=>$ip_address,
+                        'ip_desc'=>gethostbyaddr($_SERVER['REMOTE_ADDR']),
+                        'user_id'=>Auth::user()->id
+                     ]);
+        }
+
+        //save to login logs
+        DB::table('login_logs')
+        ->insert([
+            'user_id'=>Auth::user()->id,
+            'ip_id'=>$ip_id,
+            'date_created'=>date('Y-m-d H:i:s')
+        ]);
+
+    }
+
+}
